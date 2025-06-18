fd98608b15268fcb96b685086bda7dde0c963a38
jhonatan-35 <jhonatanyugsanpomatoca@gmail.com>
2025-06-18 18:12:53 -0500

    dhnn

--- project/scripts/contact.js
+++ project/scripts/contact.js
@@ -111,3 +111,22 @@ function limpiarHTML() {
         contenedorCarrito.removeChild(contenedorCarrito.firstChild)
     }
 }
+const aside = document.getElementById ("currenly-visited");
+const lastVisit = localStorage.getItem("lastVisit");
+const currentTime = Date.now();
+
+    if (!lastVisit) {
+    aside.textContent = "Welcome to our website.";
+    } else {
+    const diffTime = currentTime - Number(lastVisit);
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    if (diffDays < 1) {
+        aside.textContent = "Thank you for visited our webpage";
+    } else if (diffDays === 1) {
+        aside.textContent = "You last visited 1 day ago.";
+    } else {
+        aside.textContent = `You last visited ${diffDays} days ago.`;
+    }
+    }
+
+localStorage.setItem("lastVisit", currentTime);
